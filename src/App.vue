a949f69df08e0dfdef7d7f1b543cca4280279e8a
Monikahtp <124648489+Monikahtp@users.noreply.github.com>
2023-09-24 07:46:06 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,128 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <div class="logo">
+        <p>FreeLanco</p><br/>
+        </div>
+        <div class="kanan">
+        <RouterLink to="#home">Home</RouterLink>
+        <RouterLink to="#konten">Konten</RouterLink>
+        <RouterLink to="#meetteam">Meet Team</RouterLink>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+header {
+  line-height: 1.5;
+  /* max-height: 100vh; */
+  display: block;
+}
+
+header .wrapper{
+    display: block;
+    /* place-items: flex-start; */
+    flex-wrap: wrap;
+  }
+
+.logo {
+  display: block;
+  /* margin: 0 auto 2rem; */
+  width: 50%;
+  height: 40px;
+}
+
+.logo p{
+  font-weight: 800;
+}
+
+nav {
+  width: 100%;
+  font-size: 20px;
+  z-index: 1000;
+  /* text-align: center; */
+  /* text-align: left; */
+  margin-left: -1rem;
+  /* font-size: 1rem; */
+  padding: 1rem 9rem 1rem 1rem;
+  display: flex;
+  position:fixed;
+  background-color: white;
+
+}
+
+nav a.router-link-exact-active {
+  color: #5D5FEF;
+}
+
+nav a.router-link-exact-active:hover {
+  color: #5D5FEF;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+/* @media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+} */
+</style>
+
+<script>
+export default {
+  created() {
+    // Tangani navigasi ke anchor saat komponen dibuat
+    this.handleAnchorNavigation();
+  },
+  watch: {
+    $route(to) {
+      // Tangani navigasi ke anchor saat rute berubah
+      this.handleAnchorNavigation();
+    },
+  },
+  methods: {
+    handleAnchorNavigation() {
+      const targetHash = this.$route.hash;
+      if (targetHash) {
+        const targetElement = document.querySelector(targetHash);
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,259 @@
+<template>
+<div id="home">
+  <div class="kiri">
+    <h3>Halo, Selamat Datang</h3>
+    <h1>Temukan Pekerjaan freelance impian mu</h1>
+    <p>Bekerjalah ditempat Yang membuatmu nyaman 
+  <br>Daftar secara gratis</p>
+    <div class="tombol">
+      <div class="kiri2">
+        <button type="submit"><p>Daftar</p></button>
+      </div>
+      <div class="kanan2">
+        <button type="submit"><p>Login</p></button>
+      </div>
+    </div>
+  </div>
+  <div class="kanan">
+    <img srcset="@/assets/gambar.jpg" alt="" width="90%">
+  </div>
+</div>
+<div id="konten">
+  <div class="container">
+  <h1 style="text-align: center; font-size: 48px; font-family: Poppins;font-weight: 800; word-wrap: break-word">Konten</h1>
+  <div class="gambar">
+    <div class="kiri3">
+      <img src="https://www.creative-tim.com/blog/content/images/wordpress/2019/09/vue-js-header.jpg" alt="" style="width: 600px; height: 395px;">
+    </div>
+    <div class="kanan3">
+      <div class="judul">
+        <h1 style="color: white;font-size: 48px;font-family: Poppins;font-weight: 800;word-wrap: break-word">Karya</h1>
+      </div>
+      <div class="pic">
+        <img src="https://www.linovhr.com/wp-content/uploads/2022/12/Perbedaan-Jurnalis-dengan-Wartawan.webp" alt="">
+        <img src="https://glints.com/id/lowongan/wp-content/uploads/2020/07/Istilah-Penting-dalam-Fotografi.jpg" alt="">
+        <img src="https://awsimages.detik.net.id/community/media/visual/2023/09/07/ilustrasi-koran_169.jpeg?w=600&q=90" alt="">
+        <img src="https://images.idgesg.net/images/article/2023/06/figma_dev-mode_still-100942462-large.jpg?auto=webp&quality=85,70" alt="">
+      </div>
+  </div>
+
+  <!-- <img style="width: 600px; height: 395px; left: 90px; top: 134px; position: absolute" src="@/assets/berita.png" />
+  <img style="width: 245px; height: 217px; left: 808px; top: 251px; position: absolute" src="@/assets/berita.png" />
+  <img style="width: 245px; height: 217px; left: 1125px; top: 251px; position: absolute" src="@/assets/berita.png" />
+  <img style="width: 245px; height: 217px; left: 808px; top: 495px; position: absolute" src="@/assets/berita.png" />
+  <img style="width: 245px; height: 217px; left: 1125px; top: 495px; position: absolute" src="@/assets/berita.png" />
+  <div style="width: 562px; height: 91px; left: 808px; top: 134px; position: absolute; background: black"></div>
+  <div style="width: 487px; height: 91px; left: 845px; top: 134px; position: absolute; color: white; font-size: 48px; font-family: Poppins; font-weight: 800; word-wrap: break-word">Karya</div>
+  <div style="width: 600px; height: 282px; left: 90px; top: 573px; position: absolute; color: black; font-size: 30px; font-family: Inter; font-weight: 400; word-wrap: break-word">teks</div> -->
+  </div>
+  </div>
+</div>
+<div id="meetteam">
+  <div class="atas">
+    <h1 style="font-family: Plus Jakarta Sans">Meet Our Team</h1>
+    <p>Meet our team of professionals to serve you</p>
+    <button type="submit"><p class='about'>About Us</p></button>
+  </div>
+  <div class="bawah">
+    <div class="Group" style="width: 130%; height: 130%; position: relative">
+      <div class="WadeWarren" style="width: 184.06px; height: 40.97px; left: 75px; top: 380.23px; position: absolute; color: #454545; font-size: 21.83px; font-family: Plus Jakarta Sans; font-weight: 500; word-wrap: break-word">Jeany Aurellia</div>
+      <div class="MedicalAssistant" style="width: 155.14px; height: 26.34px; left: 105px; top: 337.22px; position: absolute; color: #C6BDBD; font-size: 14.06px; font-family: Plus Jakarta Sans; font-weight: 500; word-wrap: break-word">5027221008</div>
+      <div class="Rectangle1" style="width: 290px; height: 515px; left: 0px; top: 0px; position: absolute; border-radius: 20.08px; border: 0.87px #C3C3C3 solid"></div>
+      <img class="Rectangle2" style="width: 290px; height: 301.95px; left: 0px; top: 0px; position: absolute; border-top-left-radius: 20.08px; border-top-right-radius: 20.08px" src="@/assets/rea.jpg" />
+      <div class="hiasan">
+        <div class="Rectangle3" style="width: 28.12px; height: 31.29px; left: 90px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+        <div class="Rectangle4" style="width: 28.12px; height: 31.29px; left: 130px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+        <div class="Rectangle5" style="width: 28.12px; height: 31.29px; left: 170px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+      </div>
+    </div>
+    <div class="Group" style="width: 130%; height: 130%; position: relative">
+      <div class="WadeWarren" style="width: 184.06px; height: 40.97px; left: 60px; top: 380.23px; position: absolute; color: #454545; font-size: 21.83px; font-family: Plus Jakarta Sans; font-weight: 500; word-wrap: break-word">Monika Damelia</div>
+      <div class="MedicalAssistant" style="width: 155.14px; height: 26.34px; left: 105px; top: 337.22px; position: absolute; color: #C6BDBD; font-size: 14.06px; font-family: Plus Jakarta Sans; font-weight: 500; word-wrap: break-word">5027221011</div>
+      <div class="Rectangle1" style="width: 290px; height: 515px; left: 0px; top: 0px; position: absolute; border-radius: 20.08px; border: 0.87px #C3C3C3 solid"></div>
+      <img class="Rectangle2" style="width: 290px; height: 301.95px; left: 0px; top: 0px; position: absolute; border-top-left-radius: 20.08px; border-top-right-radius: 20.08px" src="@/assets/momon.jpg" />
+      <div class="hiasan">
+        <div class="Rectangle3" style="width: 28.12px; height: 31.29px; left: 90px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+        <div class="Rectangle4" style="width: 28.12px; height: 31.29px; left: 130px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+        <div class="Rectangle5" style="width: 28.12px; height: 31.29px; left: 170px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+      </div>
+    </div>
+    <div class="Group" style="width: 130%; height: 130%; position: relative">
+      <div class="WadeWarren" style="width: 184.06px; height: 40.97px; left: 57px; top: 380.23px; position: absolute; color: #454545; font-size: 21.83px; font-family: Plus Jakarta Sans; font-weight: 500; word-wrap: break-word">Gavriel Kurniaadi</div>
+      <div class="MedicalAssistant" style="width: 155.14px; height: 26.34px; left: 105px; top: 337.22px; position: absolute; color: #C6BDBD; font-size: 14.06px; font-family: Plus Jakarta Sans; font-weight: 500; word-wrap: break-word">5027221031</div>
+      <div class="Rectangle1" style="width: 290px; height: 515px; left: 0px; top: 0px; position: absolute; border-radius: 20.08px; border: 0.87px #C3C3C3 solid"></div>
+      <img class="Rectangle2" style="width: 290px; height: 301.95px; left: 0px; top: 0px; position: absolute; border-top-left-radius: 20.08px; border-top-right-radius: 20.08px" src="@/assets/adi.png" />
+      <div class="hiasan">
+        <div class="Rectangle3" style="width: 28.12px; height: 31.29px; left: 90px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+        <div class="Rectangle4" style="width: 28.12px; height: 31.29px; left: 130px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+        <div class="Rectangle5" style="width: 28.12px; height: 31.29px; left: 170px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+      </div>
+    </div>
+    <div class="Group" style="width: 130%; height: 130%; position: relative">
+      <div class="WadeWarren" style="width: 184.06px; height: 40.97px; left: 57px; top: 380.23px; position: absolute; color: #454545; font-size: 21.83px; font-family: Plus Jakarta Sans; font-weight: 500; word-wrap: break-word">Almendo Jekson</div>
+      <div class="MedicalAssistant" style="width: 155.14px; height: 26.34px; left: 105px; top: 337.22px; position: absolute; color: #C6BDBD; font-size: 14.06px; font-family: Plus Jakarta Sans; font-weight: 500; word-wrap: break-word">5027221073</div>
+      <div class="Rectangle1" style="width: 290px; height: 515px; left: 0px; top: 0px; position: absolute; border-radius: 20.08px; border: 0.87px #C3C3C3 solid"></div>
+      <img class="Rectangle2" style="width: 290px; height: 301.95px; left: 0px; top: 0px; position: absolute; border-top-left-radius: 20.08px; border-top-right-radius: 20.08px" src="@/assets/mendo.jpg" />
+      <div class="hiasan">
+        <div class="Rectangle3" style="width: 28.12px; height: 31.29px; left: 90px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+        <div class="Rectangle4" style="width: 28.12px; height: 31.29px; left: 130px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+        <div class="Rectangle5" style="width: 28.12px; height: 31.29px; left: 170px; top: 453.19px; position: absolute; background: #D9D9D9; border-radius: 5.03px"></div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+#home {
+  width: 100%;
+  /* height: 90vh; */
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 5rem;
+}
+
+.kiri{
+  width : 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content:end ;
+}
+.kiri h1{
+  margin-top: 10px;
+  font-size:48px;
+  font-weight: 900;
+  line-height: 1.3;
+}
+.kiri h3{
+  font-size:24px;
+  font-weight: 600;
+  color: #5D5FEF; 
+}
+
+.kiri p{
+  margin-top: 10px;
+  font-size:18px;
+}
+
+button {
+  cursor: pointer;
+  background: transparent;
+  border: none;
+}
+.tombol{
+  margin-top: 40px;
+  display: flex;
+}
+
+.tombol .kiri2{
+  background-color: #5D5FEF ;
+  border-radius: 15px;
+  padding: 1rem 4rem;
+  margin-right: 80px;
+}
+
+.tombol .kiri2 p {
+  margin-top: 0;
+  color: white;
+}
+.tombol .kanan2 p {
+  margin-top: 0;
+  color: #5D5FEF;
+
+}
+.tombol .kanan2{
+  background-color:rgba(93, 95, 239, 0.06) ;
+  border: 2px solid #5D5FEF;
+  border-radius: 15px;
+  padding: 1rem 4rem;
+}
+
+.kanan{
+  width: 50%;
+}
+
+#konten{
+  position: relative;
+  display: flex;
+  height: auto;
+
+}
+#konten .container{
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  /* padding-top: 2rem; */
+  margin-top: 42px;
+  width: 100%;
+  height: 100%;
+}
+
+.gambar{
+  display: flex;
+  flex-direction: row;
+  gap: 7rem;
+}
+
+.judul{
+  background-color: black;
+  padding-left: 20px;
+}
+
+.pic{
+  display: grid;
+  grid-template-columns: repeat(2, 2fr);
+  gap: 20px;
+  margin-top: 20px;
+}
+
+.pic img{
+  width: 245px;
+  height: 217px;
+}
+
+#meetteam{
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+#meetteam .atas{
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 15px;
+  margin: 100px 0;
+}
+
+#meetteam .atas h1{
+  font-size: 50.31px;
+  font-weight: 600;
+  font-family: 'Plus Jakarta Sans';
+}
+
+#meetteam .atas p{
+  font-size: 25px;
+  color: #797979;
+  font-family: 'Plus Jakarta Sans';
+}
+
+#meetteam .atas .about{
+  padding: 0.6rem 1rem;
+  background: #19068C;
+  color: white;
+  border-radius: 8.64px ;
+  font-size: 15.39px;
+  font-family: 'Plus Jakarta Sans';
+}
+
+#meetteam .bawah {
+  display: flex;
+}
+
+#meetteam .bawah .group {
+  width :  70% !important;
+  margin-right: 20px;
+}
+
+</style>
